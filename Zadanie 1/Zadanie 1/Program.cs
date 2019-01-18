30f7894ec39ddeb1964a253bc2969e12bb403250
gosia-mm <malgosia.mrula@gmail.com>
2019-01-18 15:31:07 +0100

    Ostatnia zmiana

--- Zadanie 1/Zadanie 1/Program.cs
+++ Zadanie 1/Zadanie 1/Program.cs
@@ -43,12 +43,49 @@ namespace Zadanie_1
             Console.WriteLine("Wartość w2 to {0}.", obj.w2);
         }
 
+        public static void SortowaniePrzezZliczanie(Kontener obj)
+        {
+            int[] tablica = { 6, 3, 6, 1, 4, 9, 0, 1, 8, 2, 6, 4, 9, 3, 7, 5, 9, 2, 7, 3, 2, 4, 1, 8, 7, 0, 8, 5, 8, 3, 6, 2, 5, 3 };
+            int[] liczniki = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // odpowiada po kolei liczbie wystąpień 0, 1, 2, 3, 4 itd.
+
+            for (int j = 0; j < liczniki.Length; j++)
+            {
+                for (int i = 0; i < tablica.Length; i++)
+                {
+                    if (tablica[i] == j)
+                        liczniki[j]++;
+                }
+            }
+
+            for (int i = 0; i < liczniki.Length; i++)
+                Console.WriteLine(liczniki[i]);
+            Console.WriteLine("----------------------------------------------------");
+
+
+            int index = 0;
+            for (int s = 0; s < liczniki.Length; s++)
+            {
+                for (int n = 0; n < liczniki[s]; n++)
+                {
+                    tablica[index++] = s;
+                }
+            }
+
+
+            for (int m = 0; m < tablica.Length; m++)
+            {
+                Console.Write(tablica[m] + " ");
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
             Kontener k = new Kontener();
             DelegateWyswietl del1 = WyswietlW1;
             DelegateWyswietl del2 = WyswietlW2;
-            DelegateWyswietl del3 = del1 + del2;
+            DelegateWyswietl del3 = SortowaniePrzezZliczanie;
 
             /*k.WyswietlCallBack(del3);
             Console.WriteLine("--");
@@ -57,42 +94,9 @@ namespace Zadanie_1
             Console.WriteLine("--");
             del3 += del2;
             k.WyswietlCallBack(del3);*/
+
+            k.WyswietlCallBack(del3);
             
-
-            int [] tablica = { 6, 3, 6, 1, 4, 9, 0, 1, 8, 2, 6, 4, 9, 3, 7, 5, 9, 2, 7, 3, 2, 4, 1, 8, 7, 0, 8, 5, 8, 3, 6, 2, 5, 3 };
-            int[] liczniki = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // odpowiada po kolei liczbie wystąpień 0, 1, 2, 3, 4 itd.
-            
-            for(int j=0; j<liczniki.Length; j++)
-            {
-                for (int i=0; i<tablica.Length; i++)
-                {
-                    if(tablica[i] == j)
-                    liczniki[j]++;
-                } 
-            }
-
-            for (int i = 0; i < liczniki.Length; i++)
-                Console.WriteLine(liczniki[i]);
-            Console.WriteLine("----------------------------------------------------");
-
-
-
-            for (int m = 0; m < tablica.Length; m++)
-            {
-                for (int s = 0; s < liczniki.Length; s++)
-                { 
-                    for (int n = 0; n < liczniki[s]; n++)
-                        tablica[n] = 0;
-                }
-
-            }
-
-
-
-            for (int m = 0; m < tablica.Length; m++)
-            {
-                Console.Write(tablica[1] + " ");
-           }
 
             Console.ReadKey();
         }
